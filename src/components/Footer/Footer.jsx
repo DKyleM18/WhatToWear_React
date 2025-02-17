09ac3c506281afa3275ebcb6465999fa39bbbfc0
DKyleM18 <dkylem18@gmail.com>
2025-02-16 23:36:20 -0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__name">Developed by Name Surname</p>
-      <p className="footer__copyright">&copy; 2024</p>
+      <p className="footer__name">Developed by Daniel Kyle Messersmith</p>
+      <p className="footer__copyright">&copy; 2025</p>
     </footer>
   );
 }
